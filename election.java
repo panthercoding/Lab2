3ceb95dd56d93c079382696357aef26481f9a439
panthercoding <79004908+panthercoding@users.noreply.github.com>
2021-02-21 14:01:46 -0600

    Create election.java

---
+++ election.java
@@ -0,0 +1,31 @@
+/*
+We explore Wisconsin and the nation as a whole in the 2020 election, looking at
+the number of Biden votes and Trump votes.
+*/
+public class election
+{
+  public static void main(String[] args)
+  {
+      //Task 1: Wisconsin
+      final int TRUMP_WISCONSIN_VOTES = 1610184;
+      final int BIDEN_WISCONSIN_VOTES = 1630866;
+      final int TOTAL_WISCONSIN_VOTES = 3298041;
+
+      double trump_wisconsin_pct = ; //finish line
+      double biden_wisconsin_pct = ; //finish line
+      double biden_wisconsin_margin = ; //finish line
+      System.out.println("Biden won by "+biden_wisconsin_margin+"% in Wisconsin"); 
+
+      //Task 2: Nation as a whole
+      final int TRUMP_VOTES = 74216154;
+      final int BIDEN_VOTES = 81268924;
+      final int TOTAL_VOTES = 158383403;
+
+      double trump_pct = ; //finish line
+      double biden_pct = ; //finish line
+      double biden_nation_margin = ; //finish line
+      System.out.println("Biden won by "+biden_nation_margin+"% nationally"); 
+
+      //Consider: Was Wisconsin more Republican or Democratic than the nation as a whole?
+  }
+}
