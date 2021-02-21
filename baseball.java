de26f9c84195a3884dae9e15949caf339ba7eedf
panthercoding <79004908+panthercoding@users.noreply.github.com>
2021-02-21 14:01:08 -0600

    Create baseball.java

---
+++ baseball.java
@@ -0,0 +1,26 @@
+/*
+We explore the baseball career of Alex Rodriguez through numbers.
+ */
+public class baseball
+{
+  public static void main(String[] args)
+  {
+    //Task 1
+    int hits = 3115;
+    int atBats = ; //complete this line
+
+    double battingAverage = ; //complete this line
+    System.out.println("ARod's career batting average was "+battingAverage);
+
+    //Task 2
+    int numSingles = 1840;
+    int numDoubles = 548;
+    int numTriples = ; //complete this line
+    int numHomeRuns = ; //complete this line
+
+    int totalBases = ; //complete this line
+    double slugPercentage = ; //complete this line
+    System.out.println("ARod's career slugging percentage was "+slugPercentage);
+
+  }
+}
